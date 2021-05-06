32a30de7d9374a2acb9ce7c572ddb3ccd0632106
Robson William <r.willup@hotmail.com>
2021-05-05 23:48:43 -0300

    Connecting to Twitter and fetching user information

--- main.go
+++ main.go
@@ -1,12 +1,29 @@
-// This app simply displays 'Hello world'
+// CodeIstari is a program to help code mentors and apprentices connect.
 package main
-
-import _ "golang.org/x/mobile/app"
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
+	"os"	
 )
 
 func main() {
-	fmt.Println("hello world")
+	userName := os.Args[1:][0]
+	url := fmt.Sprintf("%s%s%s", "https://api.twitter.com/2/users/by?usernames=", userName, "&user.fields=description")
+	client := &http.Client{}
+	req, err := http.NewRequest("GET", url, nil)
+	req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", os.Getenv("TWITTER_BEARER_TOKEN")))
+	resp, err := client.Do(req)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: %v\n", err)
+		os.Exit(1)
+	}
+	b, err := ioutil.ReadAll(resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "fetch: reading %s: %v\n", url, err)
+		os.Exit(1)
+	}
+	fmt.Printf("%s", b)
 }
